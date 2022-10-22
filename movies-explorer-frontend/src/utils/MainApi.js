1b0d039b5fca3ccfdaa44f8258f9e3bc4ca0aa3d
Varvara Korableva <v.korableva1990@yandex.ru>
2022-10-22 18:31:48 +0300

    fix

--- movies-explorer-frontend/src/utils/MainApi.js
+++ movies-explorer-frontend/src/utils/MainApi.js
@@ -1,6 +1,6 @@
-//export const BASE_URL = '//api.moviesapp.nomoredomains.sbs';
+export const BASE_URL = '//api.moviesapp.nomoredomains.sbs';
 
-export const BASE_URL = 'http://localhost:3002';
+//export const BASE_URL = 'http://localhost:3002';
 
 
   function checkResponse(res) {
